b02defe7f967a53741976353dfe16d93c42a118b
Sean Lindsey <slindsey@chapman.edu>
2019-11-20 16:33:03 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -2,18 +2,18 @@
 #numbers.sh
 #Sean Lindsey
 echo "Enter a positive number"
-read NUMBER
-$COUNT = 0
-while [ $COUNT -lt $NUMBER ]
+read -r NUMBER
+COUNT=1
+while [ "$COUNT" -lt "$NUMBER" ]
 do
-	rem=$(( n % 2 ))
-	if [ $rem -eq 0 ]; then
-		echo "$NUMBER EVEN"
-	else [ $rem -ne 0 ]; then
-		echo "$NUMBER ODD"
-		$COUNT += 1
+	rem=$(( COUNT%2 ))
+	if [ $rem -eq 0 ]
+	then
+		echo "$COUNT EVEN"
+	else
+		echo "$COUNT ODD"
 	fi
+	COUNT=$((N+1))
 done
 exit
-
 
